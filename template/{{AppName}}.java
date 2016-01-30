16404cf2f39968598c82464b8e485978703153d3
R. Dragone <rdragone@asifade.in>
2016-01-30 07:50:05 -0500

    Initial commit

---
+++ template/{{AppName}}.java
@@ -0,0 +1,12 @@
+/**
+ * @author      {{Author}} <{{Email}}>
+ * @version     1.0
+ * @since       {{Date}}
+ *
+ * {{Description}}
+ */
+
+public class {{AppName}} {
+  public static void main(String[] strArg) {
+  }
+}
